686f7aba5d42060c032099fe4beb1917860edae8
redeemer-zz <cyril.oswald@fs.cvut.cz>
2024-07-31 11:04:45 +0200

    Initial program commit

--- install.sh
+++ install.sh
@@ -22,39 +22,73 @@
 #  fi
 #fi
 
+#PWD=$(pwd)
+#
+#if [ ! -f "$PWD"/.venv/bin/python3.9 ]; then
+#  if [ -d "$PWD"/.venv ]; then
+#    echo "Directory .venv found but doesn't contains python3.9, removing .venv"
+#    rm -rf "$PWD"/.venv
+#  fi
+#  echo "Creating virtual environment"
+#
+#  if ! hash pyenv; then
+#    curl https://pyenv.run | bash
+#    export PYENV_ROOT="$HOME/.pyenv"
+#    export PATH="$PYENV_ROOT/bin:$PATH"
+#    eval "$(pyenv init --path)"
+#    eval "$(pyenv virtualenv-init -)"
+#    eval "$(pyenv init -)"
+#  fi
+#  pyenv virtualenv 3.9 infoboard_venv
+#fi
+##
+##
+##else
+##  echo "python3.9 virtual environment found"
+##fi
+##
+##source "$PWD"/.venv/bin/activate
+##
+##echo "Upgrading pip"
+##pip install --upgrade pip
+##echo "Installing requirements"
+##pip install -r "$PWD"/requirements.txt
+##
+##echo "Starting python install script..."
 PWD=$(pwd)
 
-if [ ! -f "$PWD"/.venv/bin/python3.9 ]; then
-  if [ -d "$PWD"/.venv ]; then
-    echo "Directory .venv found but doesn't contains python3.9, removing .venv"
-    rm -rf "$PWD"/.venv
-  fi
-  echo "Creating virtual environment"
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
 
-  if ! hash pyenv; then
-    curl https://pyenv.run | bash
-    export PYENV_ROOT="$HOME/.pyenv"
-    export PATH="$PYENV_ROOT/bin:$PATH"
-    eval "$(pyenv init --path)"
-    eval "$(pyenv virtualenv-init -)"
-    eval "$(pyenv init -)"
-  fi
-  pyenv virtualenv 3.9 infoboard_venv
+if ! hash pyenv; then
+  echo "installing pyenv"
+  sudo apt update
+  sudo apt install -y curl
+  curl https://pyenv.run | bash
+  eval "$(pyenv init --path)"
+  eval "$(pyenv virtualenv-init -)"
+  eval "$(pyenv init -)"
 fi
-#
-#
-#else
-#  echo "python3.9 virtual environment found"
-#fi
-#
-#source "$PWD"/.venv/bin/activate
-#
-#echo "Upgrading pip"
-#pip install --upgrade pip
-#echo "Installing requirements"
-#pip install -r "$PWD"/requirements.txt
-#
-#echo "Starting python install script..."
+
+if [ ! -d "$PYENV_ROOT/versions/infoboard_venv" ]; then
+   echo "Creating virtual environment"
+   if [ ! -d "$PYENV_ROOT/versions/3.9.2" ]; then
+      echo "Installing Python 3.9.2"
+      sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
+      libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
+      libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev
+      pyenv install 3.9.2
+   fi
+   pyenv virtualenv 3.9.2 infoboard_venv
+fi
+
+pyenv activate infoboard_venv
+echo "Upgrading pip"
+pip install --upgrade pip
+echo "Installing requirements"
+pip install -r "$PWD"/requirements.txt
+
+echo "Starting python install script..."
 
 
 
