6d15a0d3e8ca8c986b2886deb2a7c91366a56462
redeemer-zz <cyril.oswald@fs.cvut.cz>
2024-08-01 15:01:26 +0200

    Initial program commit

--- src/infoboard/configuration.py
+++ src/infoboard/configuration.py
@@ -22,6 +22,23 @@ if data_cfg_file is None:
     data_cfg_file = os.path.join(ROOT_FOLDER, 'data', 'configuration.yaml')
 
 data_cfg_file = os.path.abspath(data_cfg_file)
+auto_power_off = cfg.get('auto_power_off')
+if auto_power_off is not None:
+    apo_time = auto_power_off.split(':')
+    failed = True
+    if len(apo_time) == 2:
+        try:
+            apo_hours = int(apo_time[0])
+            apo_minutes = int(apo_time[1])
+        except ValueError:
+            pass
+        else:
+            if apo_hours >= 0 and apo_hours < 24 and apo_minutes >= 0 and apo_minutes < 60:
+                failed = False
+
+    if failed:
+        print('Auto power off format is wrong it should be HH:MM')
+        exit()
 
 if not os.path.isfile(data_cfg_file):
     print('Data configuration file doesn\'t exist, creating new one based on template')
@@ -38,9 +55,15 @@ if not os.path.isfile(data_cfg_file):
         yaml.dump(cfg_template, cfg_file)
 
 cron = CronTab(user='root')
-cron.remove_all(comment='infobar-pi')
-job = cron.new(command=f'/bin/bash {ROOT_FOLDER}/src/scripts/checker.sh > /var/log/infoboard.log 2>&1', comment='infobar-pi')
+cron.remove_all(comment='infoboard-pi')
+job = cron.new(command=f'/bin/bash {ROOT_FOLDER}/src/scripts/checker.sh > /var/log/infoboard.log 2>&1', comment='infoboard-pi')
 job.minute.every(1)
+if auto_power_off is not None:
+    job = cron.new(command=f'poweroff',
+                   comment='infoboard-pi')
+    job.hours = apo_hours
+    job.minutes = apo_minutes
+
 cron.write()
 
 
