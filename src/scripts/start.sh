db37c251c22860839e43d626248ac335fb4d1aa4
redeemer-zz <cyril.oswald@fs.cvut.cz>
2024-08-01 13:49:05 +0200

    Initial program commit

--- src/scripts/start.sh
+++ src/scripts/start.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
-sudo -u pi sh -c export PYENV_ROOT="$HOME/.pyenv"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 if ! timeout 1s xset q &>/dev/null; then
   sudo chmod 777 /dev/tty7
   sleep 20
   ./src/infoboard/configuration.py
-  sudo -u pi startx "$PYENV_ROOT"/versions/infoboard_venv/bin/python "$PWD"/../infoboard/main.py -- vt7
+  sudo -u pi startx "$HOME"/.pyenv/versions/infoboard_venv/bin/python "$PWD"/../infoboard/main.py -- vt7
 else
-  sudo -u pi "${PYENV_ROOT}"/versions/infoboard_venv/bin/python  "$PWD"/../infoboard/main.py
+  sudo -u pi "$HOME"/.pyenv/versions/infoboard_venv/bin/python  "$PWD"/../infoboard/main.py
 fi
 
 
