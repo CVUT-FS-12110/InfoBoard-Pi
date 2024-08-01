a191b71dc40d1d13a29f03d68077232913a6715e
redeemer-zz <cyril.oswald@fs.cvut.cz>
2024-08-01 14:47:28 +0200

    Initial program commit

--- src/scripts/start.sh
+++ src/scripts/start.sh
@@ -5,9 +5,7 @@ HOME_DIR=$(eval "echo ~pi")
 pgrep -f "X"
 if ! pgrep -f "X" &>/dev/null; then
   sudo chmod 777 /dev/tty7
-  sleep 20
-  ./src/infoboard/configuration.py
-  sudo -u pi startx "$HOME_DIR"/.pyenv/versions/infoboard_venv/bin/python "$PWD"/../infoboard/main.py -- vt7
+  startx "$HOME_DIR"/.pyenv/versions/infoboard_venv/bin/python "$PWD"/../infoboard/main.py -- vt7
 else
   sudo -u pi QT_QPA_PLATFORM=offscreen "$HOME_DIR"/.pyenv/versions/infoboard_venv/bin/python  "$PWD"/../infoboard/main.py
 fi
