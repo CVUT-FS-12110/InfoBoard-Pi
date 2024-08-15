b99f3ff40f6ef295e9fe8e2cf7f71809de05bc5c
redeemer-zz <cyril.oswald@fs.cvut.cz>
2024-08-15 17:30:58 +0200

    qt5 dev tools added to install script

--- src/scripts/check_server.sh
+++ src/scripts/check_server.sh
@@ -6,5 +6,5 @@ else
     PWD=$(pwd)
     SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
     HOME_DIR=$(eval "echo ~pi")
-    cd "$SCRIPT_DIR"/../infoboard/server && "$SCRIPT_DIR"/../../.venv/bin/python -m streamlit run main.py --server.port 8080
+    cd "$SCRIPT_DIR"/../infoboard/server && "$SCRIPT_DIR"/../../.venv/bin/python -m streamlit_no_pyarrow.py run main.py --server.port 8080
 fi
