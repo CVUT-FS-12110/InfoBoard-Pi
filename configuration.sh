c4c1c1091a3e59f26244cbedcb5db1bf0dfa81de
redeemer-zz <cyril.oswald@fs.cvut.cz>
2024-08-16 11:20:49 +0200

    fix default media dir

--- configuration.sh
+++ configuration.sh
@@ -4,26 +4,26 @@ PWD=$(pwd)
 
 if [ ! -f "$PWD"/src/infoboard/configuration.py ]; then
   echo "Infoboard Pi source code doesn't found, start thi script inside the repository directory."
-  return
+  exit
 else
   echo "Infoboard Pi source code found"
 fi
 
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
+#export PYENV_ROOT="$HOME/.pyenv"
+#export PATH="$PYENV_ROOT/bin:$PATH"
 
-if ! hash pyenv; then
-  echo "Pyenv not found, start install.sh..."
-  return
-fi
+#if ! hash pyenv; then
+#  echo "Pyenv not found, start install.sh..."
+#  exit
+#fi
 
-if [ ! -d "$PYENV_ROOT/versions/infoboard_venv" ]; then
+if [ ! -f ".venv/bin/python" ]; then
   echo "Virtual environment not found, start install.sh..."
   return
 
 fi
 
-sudo "${PYENV_ROOT}"/versions/infoboard_venv/bin/python ./src/infoboard/reconfiguration.py
+sudo ./.venv/bin/python ./src/infoboard/reconfiguration.py
 
 
 
