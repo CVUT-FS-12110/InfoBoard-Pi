b3b2a31681880b3f3564ddaaab6c1f77b9c3321c
redeemer-zz <cyril.oswald@fs.cvut.cz>
2024-08-16 11:23:29 +0200

    fix default media dir

--- src/infoboard/reconfiguration.py
+++ src/infoboard/reconfiguration.py
@@ -38,7 +38,7 @@ if default_media_dir is None:
     default_media_dir = os.path.join(ROOT_FOLDER, 'data')
     print(f'Media default directory doesn\'t found in the config.yaml, fallback to the default {default_media_dir}')
 
-
+print(default_media_dir)
 auto_power_off = cfg.get('auto_power_off')
 
 if auto_power_off is not None:
@@ -84,18 +84,14 @@ if not os.path.isfile(DATA_CFG_FILE):
 else:
     with open(DATA_CFG_FILE, 'r') as cfg_file:
         cfg_data = yaml.safe_load(cfg_file)
-    updated = False
     if 'auto_update_data_config' in cfg.keys():
         cfg_data['auto_update'] = cfg.get('auto_update_data_config')
-        updated = True
     if 'default_slide_time' in cfg.keys():
         cfg_data['default_slide_time'] = cfg.get('default_slide_time')
-        updated = False
     cfg_data['default_media_dir'] = os.path.realpath(default_media_dir)
-    if updated:
-        print('Updating data config file ...')
-        with open(DATA_CFG_FILE, 'w') as cfg_file:
-            yaml.dump(cfg_data, cfg_file)
+    print('Updating data config file ...')
+    with open(DATA_CFG_FILE, 'w') as cfg_file:
+        yaml.dump(cfg_data, cfg_file)
 
 
 print('Setting cron jobs ...')
