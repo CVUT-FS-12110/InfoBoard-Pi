c2bae27115ecd592470c6cd6f4e6b43d8b207184
redeemer-zz <cyril.oswald@fs.cvut.cz>
2024-09-12 14:40:49 +0200

    systemd support

---
+++ install_services.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+PWD=$(pwd)
+
+if [ ! -f "$PWD"/src/infoboard/configuration.py ]; then
+  echo "Infoboard Pi source code doesn't found, start thi script inside the repository directory."
+  exit
+else
+  echo "Infoboard Pi source code found"
+fi
+
+echo "Installing services"
+
+sudo cp "$PWD"/src/scripts/infoboard-pi.service /etc/systemd/system/infoboard-pi.service
+sudo cp "$PWD"/src/scripts/infoboard-pi-server.service /etc/systemd/system/infoboard-pi-server.service
+
+echo exec "$PWD"/src/scripts/start.sh >> /usr/local/sbin/infoboardPi.sh
+echo exec "$PWD"/src/scripts/check_server.sh >> /usr/local/sbin/infoboardPiServer.sh
+
+sudo systemctl daemon-reload
